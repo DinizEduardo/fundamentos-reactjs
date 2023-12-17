7288e737f28cbfd99e07cb499d0926b327722bbe
dinizeduardo <eduardo.diniz987@gmail.com>
2023-12-16 23:44:39 -0300

    feat: propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { Post } from "./Post"
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Eduardo Diniz" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ipsa ducimus necessitatibus ab, esse et ratione odit nulla itaque quibusdam, maxime unde tenetur doloremque repudiandae excepturi animi nostrum? Ex, placeat?" 
+      />
     </div>
   )
 }
