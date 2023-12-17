2c643d97c20d66156cd74fdb1bc9796f4acecc0f
dinizeduardo <eduardo.diniz987@gmail.com>
2023-12-17 00:35:51 -0300

    feat: sidebar basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
-
-      <Post 
-        author="Eduardo Diniz" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ipsa ducimus necessitatibus ab, esse et ratione odit nulla itaque quibusdam, maxime unde tenetur doloremque repudiandae excepturi animi nostrum? Ex, placeat?" 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Eduardo Diniz" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ipsa ducimus necessitatibus ab, esse et ratione odit nulla itaque quibusdam, maxime unde tenetur doloremque repudiandae excepturi animi nostrum? Ex, placeat?" 
+          />
+        </main>
+      </div>
+      
     </div>
   )
 }
